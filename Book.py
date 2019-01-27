dfc10889140f656600ac64f9160bad2157bfed39
Yarosh-Oleh-04 <yarosholeh04@gmail.com>
2019-01-27 11:34:07 +0200

    add class book

--- Book.py
+++ Book.py
@@ -1,2 +1,26 @@
 class Book:
-    pass
+    def __init__ (self, name, author, publisher, year, is_available, cut_num):
+        self.name = name
+        self.author = author
+        self.publisher = publisher
+        self.year = year
+        self.is_available = is_available
+        self.cut_num = cut_num
+        
+    def __str__ (self):
+        return self.name + ', ' + self.author + ', ' + self.publisher + ', ' + str(self.year) + ', \nAvailable: ' + str(self.is_available) +',Catalog number: ' + str(self.cut_num)
+        
+    def __eq__ (self, other):
+        return (self.name == other.name) and (self.author == other.author)
+    
+    def __ne__ (self, other):
+        return not self == other
+    
+    def print(self):
+        print(str(self))
+        
+book = Book('AAA', 'aaa', 'OOO', 1980, True, 2211006655)
+book.print()
+book1 = Book('AAA', 'ooo', 'QQQ', 1980, True, 2211006655)
+
+print(book != book1)
