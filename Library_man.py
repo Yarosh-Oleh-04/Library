aeae0c3aa4d1db8eb88566e644f14d24fda3cc8a
YuriyZadereckiy <yurazadereckii@gmail.com>
2019-01-27 12:17:33 +0200

    More work on Lib_Man

--- Library_man.py
+++ Library_man.py
@@ -1,15 +1,28 @@
 import Book
 import Library
 
-LIBRARY_ROOT="/Books"
+LIBRARY_ROOT="Books/books.txt"
 
 def Choose(opt):
-    lib = Library.Library()
+    lib = Library.Library(LIBRARY_ROOT)
     
     if opt=='1':
+        list = "\n".join(getBooks())
+        print(list)
         pass
     elif opt =='2':
-        pass
+        print("Enter book name:")
+        name = input('=>')
+        book = name.split(',')
+        if lib.hasBook(Book(book[0], book[1], '', 0, True, 111)):
+            print("A book found:")
+            
+            books = lib.findBooks(name=book[0], author=book[1])
+            for b in books:
+                b.print()
+        else:
+            print("No match found.")
+        
     elif opt == '3':
         pass
     elif opt == '4':
@@ -18,12 +31,14 @@ def Choose(opt):
         lib.storeBooks()
         print("Thanks for using our services. Good bye!")
         exit()
+    else:
+        print("No such option available.")
     pass
 
 while True:
     print("Welcome to the library!/n Choose an option:")
     print("""
-1. See all books
+1. Show all books
 2. Look for a book
 3. Borrow a book
 4. Return a book
