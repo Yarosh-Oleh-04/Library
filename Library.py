19d5f10f9dbeb5e798b0b43f0ad4903bf17c0946
YuriyZadereckiy <yurazadereckii@gmail.com>
2019-01-27 11:46:22 +0200

    Added library mocks

--- Library.py
+++ Library.py
@@ -1,2 +1,20 @@
+import Book
+
 class Library:
-    pass
+    def getBooks(self):
+        pass
+    
+    def storeBooks(self):
+        pass
+    
+    def addBook(self, book):
+        pass
+    
+    def hasBook(self, book):
+        pass
+    
+    def borrowBook(self, book):
+        pass
+    
+    def findBooks(self, author='', name='', publisher='', year='', cat_num='', is_available=''):
+        pass
