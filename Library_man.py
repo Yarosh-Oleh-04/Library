1bc4f33c793015b5209c32ffeaf5604c147f954c
YuriyZadereckiy <yurazadereckii@gmail.com>
2019-01-30 20:27:42 +0200

    More work on LibMan

--- Library_man.py
+++ Library_man.py
@@ -3,31 +3,60 @@ import Library
 
 LIBRARY_ROOT="Books/books.txt"
 
+def printBooks(books):
+    ln = len(books)
+    for i in range(ln): print(i, books[i], sep=': ')
+    
+def lookForABook(lib):
+    print("Enter book name:")
+    name = input('=>')
+    
+    foundBooks = lib.findBooks(name=name)
+    
+    if(foundBooks is not None and foundBooks.any()):
+       print("Books found:")
+       printBooks(foundBooks)
+    else:
+        print("No match found.")
+        
+    return foundBooks
+    
+def borrowABook(books):
+    print('Choose a book to borrow:')
+    num = int(input("=>"))
+    
+    book = books[num]
+    book.isAvailable = False
+    
+    print("Book" + str(book) +" borrowed!")
+
 def Choose(opt):
     lib = Library.Library(LIBRARY_ROOT)
-    list = lib.getBooks()
+    libBooks = lib.getBooks()
+    foundBooks = []
     
+    # print all
     if opt=='1':
-        for i in list: print(i)
+        foundBooks = libBooks.copy()
+        printBooks(foundBooks)
+        
+    # look for a book
     elif opt =='2':
-        print("Enter book name:")
-        name = input('=>')
-        if lib.hasBook(Book.Book(name, '', '', '', '', '')):
-            print("A book found:")
-            
-            books = lib.findBooks(name=name)
-            
-            if(books is not None and books.any()):
-               for b in books:
-                   b.print()
-        else:
-            print("No match found.")
+        foundBooks = lookForABook(lib)
         
     elif opt == '3':
-        pass
+        while foundBooks is None or len(foundBooks) <= 0:
+            foundBooks = lookForABook(lib)
+            
+        borrowABook(foundBooks)
+        
     elif opt == '4':
         pass
+    
     elif opt == '5':
+        pass
+    
+    elif opt == '6':
         lib.storeBooks()
         print("Thanks for using our services. Good bye!")
         exit()
@@ -44,7 +73,8 @@ while True:
 2. Look for a book
 3. Borrow a book
 4. Return a book
-5. Exit
+5. Search by parameters
+6. Exit
 """)
     
     opt = input("=>")
