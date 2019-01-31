d846b9de00c3e7c829070a85665a476e21f0dd14
Yarosh-Oleh-04 <yarosholeh04@gmail.com>
2019-01-31 20:21:29 +0200

    ...

--- Library.py
+++ Library.py
@@ -1,33 +1,34 @@
 import Book
 
+
 class Library:
-    
-    def __init__ (self, root):
+
+    def __init__(self, root):
         self.root = root
         self.books = []
-    
+
     def getBooks(self):
         books = open(self.root, 'r').readlines()
         for i in books:
             elem = i.split(',')
             self.books += [Book.Book(elem[0], elem[1], elem[2], elem[3], elem[4], elem[5])]
         return self.books
-    
+
     def storeBooks(self):
         pass
-    
+
     def addBook(self, book):
         pass
-    
+
     def hasBook(self, book):
         for i in self.books:
             if i.name == book.name:
                 return True
         return False
-    
+
     def borrowBook(self, book):
         pass
-    
+
     def findBooks(self, name='', author='', publisher='', year='', cut_num='', is_available=''):
         similar = []
         n = 0
@@ -41,8 +42,8 @@ class Library:
             if n == 6: similar += [i]
             n = 0
         return similar
-    
-#LIBRARY_ROOT="Books/books.txt"
-#lib = Library(LIBRARY_ROOT)
-#lib.getBooks()
-#print(str(lib.hasBook(Book.Book('Цифрова фортеця','Ден Браун','','','',''))))
+
+# LIBRARY_ROOT="Books/books.txt"
+# lib = Library(LIBRARY_ROOT)
+# lib.getBooks()
+# print(str(lib.hasBook(Book.Book('Цифрова фортеця','Ден Браун','','','',''))))
--- Library_man.py
+++ Library_man.py
@@ -1,68 +1,71 @@
-import Book
 import Library
 
-LIBRARY_ROOT="Books/books.txt"
+LIBRARY_ROOT = "Books/books.txt"
 
 def printBooks(books):
     ln = len(books)
     for i in range(ln): print(i + 1, books[i], sep=': ')
-    
+
+
 def lookForABook(lib):
     print("Enter book name:")
     name = input('=>')
-    
+
     foundBooks = lib.findBooks(name=name)
-    
-    if foundBooks != []:
-       print("Books found:")
-       printBooks(foundBooks)
+
+    if foundBooks:
+        print("Books found:")
+        printBooks(foundBooks)
     else:
         print("No match found.")
-        
+
     return foundBooks
-    
+
+
 def borrowABook(books):
     print('Choose a book to borrow:')
     num = int(input("=>"))
-    
+
     book = books[num]
     book.isAvailable = False
-    
-    print("Book" + str(book) +" borrowed!")
 
-def Choose(opt):
+    print("Book" + str(book) + " borrowed!")
+
+
+def Choose(option):
     lib = Library.Library(LIBRARY_ROOT)
     libBooks = lib.getBooks()
     foundBooks = []
-    
+
     # print all
-    if opt=='1':
+    if option == '1':
         foundBooks = libBooks.copy()
         printBooks(foundBooks)
-        
+
     # look for a book
-    elif opt =='2':
-        foundBooks = lookForABook(lib)
-        
-    elif opt == '3':
+    elif option == '2':
+        lookForABook(lib)
+
+    elif option == '3':
         while foundBooks is None or len(foundBooks) <= 0:
             foundBooks = lookForABook(lib)
-            
+
         borrowABook(foundBooks)
-        
-    elif opt == '4':
+
+    elif option == '4':
         pass
-    
-    elif opt == '5':
+
+    elif option == '5':
         pass
-    
-    elif opt == '6':
+
+    elif option == '6':
         lib.storeBooks()
         print("Thanks for using our services. Good bye!")
         exit()
     else:
         print("No such option available.")
     pass
+
 
 print("Welcome to the library!")
 
@@ -76,9 +79,6 @@ while True:
 5. Search by parameters
 6. Exit
 """)
-    
+
     opt = input("=>")
     Choose(opt)
-    
-    
-    
